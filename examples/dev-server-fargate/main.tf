04f3ba9388f22afb937d2b22c20c704379c731ef
Luke Kysow <1034429+lkysow@users.noreply.github.com>
2021-05-25 08:40:38 -0700

    Use only availability zones
    Filters out local zones

--- examples/dev-server-fargate/main.tf
+++ examples/dev-server-fargate/main.tf
@@ -11,7 +11,12 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 
 data "aws_caller_identity" "this" {}
 
