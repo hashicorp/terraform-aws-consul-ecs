35d05e8f76c61cfcc3e3463091837167d9fca056
Paul Glass <pglass@hashicorp.com>
2021-10-25 17:26:35 -0500

    Spread NAT gateways across AZs

--- test/acceptance/setup-terraform/main.tf
+++ test/acceptance/setup-terraform/main.tf
@@ -18,13 +18,24 @@ resource "random_string" "suffix" {
   special = false
 }
 
+resource "random_shuffle" "azs" {
+  input = data.aws_availability_zones.available.names
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.78.0"
 
-  name                 = local.name
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
+  name = local.name
+  cidr = "10.0.0.0/16"
+  // The NAT gateway limit is per AZ. With `single_nat_gateway = true`, the NAT gateway is created
+  // in the first public subnet. Shuffling AZs helps spread NAT gateways across AZs to help with this.
+  azs = [
+    // Silly, but avoids this error: `"count" value depends on resource attributes that cannot be determined until apply`
+    random_shuffle.azs.result[0],
+    random_shuffle.azs.result[1],
+    random_shuffle.azs.result[2],
+  ]
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_nat_gateway   = true
