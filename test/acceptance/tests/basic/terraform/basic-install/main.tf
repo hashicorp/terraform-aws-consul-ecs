d9c70d991c41595b2e5eb6609d80640b8ac8417d
Paul Glass <pglass@hashicorp.com>
2022-06-09 12:14:58 -0500

    Fix passing in roles (#113)
    The previous behavior of detecting null task_role / execution_role
    inputs to mesh-task relied on a bug in Terraform 1.0 which was fixed in
    1.1. This meant you could not reliably pass in roles with Terraform 1.1.
    
    The issue is fixed by adding `create_task_role` and
    `create_execution_role` input flags to mesh-task. These default to true
    so that we create these roles by default. These must be set to false to
    use existing roles.
    
    The mesh-task behavior is also changed to NOT configure roles that are
    passed in. If a role is passed in, it is the responsibility of the user
    to ensure the role is correctly configured. Validation is added to
    disallow passing `additional_task_role_policies` and
    `additional_execution_role_policies` when `create_task_role=false` and
    `create_execution_role=false`, respectively.
    
    Also, bump Terraform to 1.2.2 in acceptance tests

--- test/acceptance/tests/basic/terraform/basic-install/main.tf
+++ test/acceptance/tests/basic/terraform/basic-install/main.tf
@@ -226,7 +226,7 @@ EOT
   application_shutdown_delay_seconds = 10
 
   tls                       = var.secure
-  consul_server_ca_cert_arn = module.consul_server.ca_cert_arn
+  consul_server_ca_cert_arn = var.secure ? module.consul_server.ca_cert_arn : ""
   gossip_key_secret_arn     = var.secure ? aws_secretsmanager_secret.gossip_key[0].arn : ""
   acls                      = var.secure
   consul_ecs_image          = var.consul_ecs_image
@@ -283,7 +283,7 @@ module "test_server" {
   port = 9090
 
   tls                       = var.secure
-  consul_server_ca_cert_arn = module.consul_server.ca_cert_arn
+  consul_server_ca_cert_arn = var.secure ? module.consul_server.ca_cert_arn : ""
   gossip_key_secret_arn     = var.secure ? aws_secretsmanager_secret.gossip_key[0].arn : ""
   acls                      = var.secure
   consul_ecs_image          = var.consul_ecs_image
@@ -291,12 +291,19 @@ module "test_server" {
   consul_http_addr         = var.secure ? "https://${module.consul_server.server_dns}:8501" : ""
   consul_https_ca_cert_arn = var.secure ? module.consul_server.ca_cert_arn : ""
 
-  additional_task_role_policies = [aws_iam_policy.execute-command.arn]
+  // Test passing in roles. This requires users to correctly configure the roles outside mesh-task.
+  create_task_role      = false
+  create_execution_role = false
+  task_role             = aws_iam_role.task
+  execution_role        = aws_iam_role.execution
+
 }
 
-// Testing passing task/execution role into mesh-task
+// Configure a task role for passing in to mesh-task.
 resource "aws_iam_role" "task" {
   name = "test_server_${var.suffix}_task_role"
+  path = "/consul-ecs/"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -318,6 +325,35 @@ resource "aws_iam_role" "task" {
   }
 }
 
+// Policy to allow iam:GetRole (required for auth method)
+resource "aws_iam_policy" "get-task-role" {
+  count = var.secure ? 1 : 0
+  name  = "test_server_${var.suffix}_get_task_role_policy"
+  path  = "/consul-ecs/"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iam:GetRole"
+      ],
+      "Resource": [
+        "${aws_iam_role.task.arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "get-task-role" {
+  count      = var.secure ? 1 : 0
+  role       = aws_iam_role.task.id
+  policy_arn = aws_iam_policy.get-task-role[count.index].arn
+}
 
 // Policy to allow `aws execute-command`
 resource "aws_iam_policy" "execute-command" {
@@ -347,6 +383,11 @@ EOF
   # tags = var.tags
 }
 
+resource "aws_iam_role_policy_attachment" "execute-command" {
+  role       = aws_iam_role.task.id
+  policy_arn = aws_iam_policy.execute-command.arn
+}
+
 resource "aws_iam_role" "execution" {
   name = "test_server_${var.suffix}_execution_role"
   path = "/consul-ecs/"
@@ -363,6 +404,37 @@ resource "aws_iam_role" "execution" {
       }
     ]
   })
+
+  inline_policy {
+    name   = "test_server_${var.suffix}_execution_role_policy"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+%{if var.secure~}
+    {
+      "Effect": "Allow",
+      "Action": [
+        "secretsmanager:GetSecretValue"
+      ],
+      "Resource": [
+        "${module.consul_server.ca_cert_arn}",
+        "${aws_secretsmanager_secret.gossip_key[0].arn}"
+      ]
+    },
+%{endif~}
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+  }
 }
 
 locals {
---
+++ test/acceptance/tests/basic/terraform/pass-role-additional-policies-validate/main.tf
@@ -0,0 +1,55 @@
+// We test this with a 'terraform plan' only.
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+variable "test_execution_role" {
+  description = "By default, we test with the task role. Set this to true to test with the execution role."
+  type        = bool
+  default     = false
+}
+
+
+locals {
+  container_definitions = [{
+    name  = "basic"
+    image = "fake"
+  }]
+}
+
+module "task_role_test" {
+  count = var.test_execution_role ? 0 : 1
+
+  source                = "../../../../../../modules/mesh-task"
+  family                = "family-1"
+  log_configuration     = null
+  container_definitions = local.container_definitions
+  retry_join            = ["test"]
+  outbound_only         = true
+
+  create_task_role              = false
+  additional_task_role_policies = ["arn:aws:iam::000000000000:policy/some-policy"]
+  task_role = {
+    id  = "my-task-role"
+    arn = "arn:aws:iam::000000000000:role/some-role"
+  }
+}
+
+module "execution_role_test" {
+  count = var.test_execution_role ? 1 : 0
+
+  source                = "../../../../../../modules/mesh-task"
+  family                = "family-2"
+  log_configuration     = null
+  container_definitions = local.container_definitions
+  retry_join            = ["test"]
+  outbound_only         = true
+
+  create_execution_role              = false
+  additional_execution_role_policies = ["arn:aws:iam::000000000000:policy/some-policy"]
+  execution_role = {
+    id  = "my-task-role"
+    arn = "arn:aws:iam::000000000000:role/some-role"
+  }
+}
