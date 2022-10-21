e186bca0fb56c5c4d50430e8ddb2d498e05e4858
Eric <eric@haberkorn.co>
2022-10-21 11:56:21 -0400

    add cluster peering support

--- modules/dev-server/variables.tf
+++ modules/dev-server/variables.tf
@@ -195,7 +195,7 @@ variable "primary_datacenter" {
 }
 
 variable "retry_join_wan" {
-  description = "List of WAN addresses to join for Consul cluster peering. Must not be provided when using mesh-gateway WAN federation."
+  description = "List of WAN addresses to join for Consul WAN federation. Must not be provided when using mesh-gateway WAN federation."
   type        = list(string)
   default     = []
 }
@@ -207,7 +207,13 @@ variable "primary_gateways" {
 }
 
 variable "enable_mesh_gateway_wan_federation" {
-  description = "Controls whether or not WAN cluster peering via mesh gateways is enabled. Default is false."
+  description = "Controls whether or not WAN federations via mesh gateways is enabled. Default is false."
+  type        = bool
+  default     = false
+}
+
+variable "enable_cluster_peering" {
+  description = "Controls whether or not cluster peering is enabled. Default is false."
   type        = bool
   default     = false
 }
