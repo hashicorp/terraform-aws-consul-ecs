25efc33e6822fb4eb294c64c30bda54d94a869d3
Paul Glass <pglass@hashicorp.com>
2022-12-07 10:03:41 -0600

    Fix index out of bounds in CloudWatch log parsing

--- test/acceptance/framework/helpers/cloudwatch.go
+++ test/acceptance/framework/helpers/cloudwatch.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"fmt"
 	"sort"
 	"strings"
 	"time"
@@ -33,9 +34,14 @@ func GetCloudWatchLogEvents(t terratestTesting.TestingT, testConfig *config.Test
 			continue
 		}
 		parts := strings.SplitN(line, "\t", 2)
+		if len(parts) < 2 {
+			err := fmt.Errorf("failed to split CloudWatch log line: %q", line)
+			t.Error(err)
+			return nil, err
+		}
 		timestamp, err := time.Parse(time.RFC3339, parts[0])
 		if err != nil {
-			t.Errorf("failed to parse timestamp in log line `%s`", line)
+			t.Errorf("failed to parse timestamp in CloudWatch log line: %q", line)
 			return nil, err
 		}
 		result = append(result, LogEvent{timestamp, parts[1]})
