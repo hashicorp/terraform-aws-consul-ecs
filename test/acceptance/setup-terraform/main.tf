89f9855c84108206e9bb1280a9fecebff495224d
Ganeshrockz <ganesh.seetharaman@hashicorp.com>
2023-07-21 12:41:27 +0530

    Fix fmt

--- test/acceptance/setup-terraform/main.tf
+++ test/acceptance/setup-terraform/main.tf
@@ -101,11 +101,11 @@ module "hcp" {
 }
 
 resource "aws_security_group_rule" "cluster_egress" {
-  description              = "Access to endpoints outside the VPC"
-  type                     = "egress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  cidr_blocks              = ["0.0.0.0/0"]
-  security_group_id        = module.vpc.default_security_group_id
+  description       = "Access to endpoints outside the VPC"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpc.default_security_group_id
 }
