276431e6e0a1fa36b3e30d6fe8964c4ead5ddce2
Ganeshrockz <ganesh.seetharaman@hashicorp.com>
2023-07-21 12:39:33 +0530

    Fix fmt

--- test/acceptance/setup-terraform/main.tf
+++ test/acceptance/setup-terraform/main.tf
@@ -106,6 +106,6 @@ resource "aws_security_group_rule" "cluster_egress" {
   from_port                = 0
   to_port                  = 0
   protocol                 = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks              = ["0.0.0.0/0"]
   security_group_id        = module.vpc.default_security_group_id
 }
