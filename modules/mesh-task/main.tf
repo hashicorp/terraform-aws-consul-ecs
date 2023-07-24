34fd370960f608b10df1b261913eeafd5c845008
Ganeshrockz <ganesh.seetharaman@hashicorp.com>
2023-07-24 22:24:05 +0530

    Fix tests :(

--- modules/mesh-task/main.tf
+++ modules/mesh-task/main.tf
@@ -207,20 +207,14 @@ resource "aws_ecs_task_definition" "this" {
             }
             secrets = concat(
               var.tls ? [
-                concat(
-                  local.https_ca_cert_arn != "" ? [
-                    {
-                      name      = "CONSUL_HTTPS_CACERT_PEM"
-                      valueFrom = local.https_ca_cert_arn
-                    }
-                  ] : [],
-                  local.grpc_ca_cert_arn != "" ? [
-                    {
-                      name      = "CONSUL_GRPC_CACERT_PEM"
-                      valueFrom = local.grpc_ca_cert_arn
-                    }
-                  ] : [],
-                ),
+                {
+                  name      = "CONSUL_HTTPS_CACERT_PEM"
+                  valueFrom = local.https_ca_cert_arn
+                },
+                {
+                  name      = "CONSUL_GRPC_CACERT_PEM"
+                  valueFrom = local.grpc_ca_cert_arn
+                }
               ] : [],
               []
             )
