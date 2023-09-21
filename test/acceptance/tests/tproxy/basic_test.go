4f2084f21f30cee01135ddf91f9296aa9dc9d424
Ganesh S <ganesh.seetharaman@hashicorp.com>
2023-09-21 15:29:12 +0530

    Add tproxy acceptance tests (#211)

---
+++ test/acceptance/tests/tproxy/basic_test.go
@@ -0,0 +1,268 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package tproxy
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/hashicorp/consul/sdk/testutil/retry"
+	"github.com/hashicorp/terraform-aws-consul-ecs/test/acceptance/framework/helpers"
+	"github.com/hashicorp/terraform-aws-consul-ecs/test/acceptance/framework/logger"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransparentProxy(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		secure        bool
+		enterprise    bool
+		stateFile     string
+		ecsClusterARN string
+		datacenter    string
+	}{
+		{secure: false},
+		{secure: true},
+		{secure: true, enterprise: true},
+	}
+
+	cfg := suite.Config()
+
+	require.Equal(t, "EC2", cfg.LaunchType, "TestTransparentProxy requires EC2 launch type for ECS.")
+	require.GreaterOrEqual(t, len(cfg.ECSClusterARNs), len(cases),
+		"TestBasic requires %d ECS clusters. Update setup-terraform and re-run.", len(cases),
+	)
+
+	initOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "./terraform",
+		NoColor:      true,
+	})
+	terraform.Init(t, initOptions)
+
+	for i, c := range cases {
+		c := c
+
+		// To support running in parallel, each test case should have:
+		// - a unique cluster
+		// - a unique Terrform state file to isolate resources
+		// - a unique datacenter within the VPC to avoid conflicts in CloudMap namespaces
+		// If more clusters are needed, update the cluster count in setup-terraform
+		c.ecsClusterARN = cfg.ECSClusterARNs[i]
+		c.datacenter = fmt.Sprintf("dc%d", i)
+		c.stateFile = fmt.Sprintf("terraform-%d.tfstate", i)
+
+		t.Run(fmt.Sprintf("secure: %t,enterprise: %t, tproxy: true", c.secure, c.enterprise), func(t *testing.T) {
+			t.Parallel()
+
+			randomSuffix := strings.ToLower(random.UniqueId())
+
+			tfEnvVars := map[string]string{
+				// Use a unique state file for each parallel invocation of Terraform.
+				"TF_CLI_ARGS": fmt.Sprintf("-state=%s -state-out=%s", c.stateFile, c.stateFile),
+			}
+
+			tfVars := cfg.TFVars("route_table_ids", "ecs_cluster_arns")
+			tfVars["secure"] = c.secure
+			tfVars["suffix"] = randomSuffix
+			tfVars["ecs_cluster_arn"] = c.ecsClusterARN
+			tfVars["consul_datacenter"] = c.datacenter
+			clientServiceName := "test_client"
+
+			serverServiceName := "test_server"
+			if c.secure {
+				// This uses the explicitly passed service name rather than the task's family name.
+				serverServiceName = "custom_test_server"
+			}
+			tfVars["server_service_name"] = serverServiceName
+
+			image := cfg.ConsulImageURI(c.enterprise)
+			t.Logf("using consul image = %s", image)
+			tfVars["consul_image"] = image
+			if c.enterprise {
+				license := os.Getenv("CONSUL_LICENSE")
+				require.True(t, license != "", "CONSUL_LICENSE not found but is required for enterprise tests")
+
+				// Pass the license via environment variable to help ensure it is not logged.
+				tfEnvVars["TF_VAR_consul_license"] = license
+			}
+
+			applyOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+				TerraformDir: initOptions.TerraformDir,
+				Vars:         tfVars,
+				NoColor:      true,
+				EnvVars:      tfEnvVars,
+			})
+
+			t.Cleanup(func() {
+				if cfg.NoCleanupOnFailure && t.Failed() {
+					logger.Log(t, "skipping resource cleanup because -no-cleanup-on-failure=true")
+				} else {
+					terraform.Destroy(t, applyOptions)
+				}
+			})
+
+			terraform.Apply(t, applyOptions)
+
+			// Wait for consul server to be up.
+			var consulServerTaskARN string
+			retry.RunWith(&retry.Timer{Timeout: 3 * time.Minute, Wait: 10 * time.Second}, t, func(r *retry.R) {
+				tasks, err := helpers.ListTasks(t, c.ecsClusterARN, cfg.Region, fmt.Sprintf("consul_server_%s", randomSuffix))
+
+				r.Check(err)
+				require.NotNil(r, tasks)
+				require.Len(r, tasks.TaskARNs, 1)
+				consulServerTaskARN = tasks.TaskARNs[0]
+			})
+
+			var controllerTaskID string
+			if c.secure {
+				retry.RunWith(&retry.Timer{Timeout: 2 * time.Minute, Wait: 30 * time.Second}, t, func(r *retry.R) {
+					tasks, err := helpers.ListTasks(t, c.ecsClusterARN, cfg.Region, fmt.Sprintf("%s-consul-ecs-controller", randomSuffix))
+
+					r.Check(err)
+					require.NotNil(r, tasks)
+					require.Len(r, tasks.TaskARNs, 1)
+
+					controllerTaskID = helpers.GetTaskIDFromARN(tasks.TaskARNs[0])
+				})
+
+				// Check controller logs to see if the anonymous token gets configured. This should
+				// indicate that the controller has created the service auth method, policies and roles.
+				retry.RunWith(&retry.Timer{Timeout: 2 * time.Minute, Wait: 30 * time.Second}, t, func(r *retry.R) {
+					appLogs, err := helpers.GetCloudWatchLogEvents(t, cfg, c.ecsClusterARN, controllerTaskID, "consul-ecs-controller")
+					require.NoError(r, err)
+
+					logMsg := "Successfully configured the anonymous token"
+					appLogs = appLogs.Filter(logMsg)
+					require.Len(r, appLogs, 1)
+					require.Contains(r, appLogs[0].Message, logMsg)
+				})
+			}
+
+			// Wait for both tasks to be registered in Consul.
+			retry.RunWith(&retry.Timer{Timeout: 6 * time.Minute, Wait: 20 * time.Second}, t, func(r *retry.R) {
+				out, err := helpers.ExecuteRemoteCommand(t, cfg, c.ecsClusterARN, consulServerTaskARN, "consul-server", `/bin/sh -c "consul catalog services"`)
+				r.Check(err)
+				if !strings.Contains(out, fmt.Sprintf("%s_%s", serverServiceName, randomSuffix)) ||
+					!strings.Contains(out, fmt.Sprintf("%s_%s", clientServiceName, randomSuffix)) {
+					r.Errorf("services not yet registered, got %q", out)
+				}
+			})
+
+			// Wait for passing health check for test_server and test_client
+			tokenHeader := ""
+			if c.secure {
+				tokenHeader = `-H "X-Consul-Token: $CONSUL_HTTP_TOKEN"`
+			}
+
+			// Wait for passing health check for `serverServiceName` and `clientServiceName`.
+			// `clientServiceName` has a check synced from ECS and a consul-dataplane check.
+			// We check if both of them are in a passing state.
+			retry.RunWith(&retry.Timer{Timeout: 2 * time.Minute, Wait: 20 * time.Second}, t, func(r *retry.R) {
+				out, err := helpers.ExecuteRemoteCommand(
+					t, cfg, c.ecsClusterARN, consulServerTaskARN, "consul-server",
+					fmt.Sprintf(`/bin/sh -c 'curl %s localhost:8500/v1/health/checks/%s_%s'`, tokenHeader, clientServiceName, randomSuffix),
+				)
+				r.Check(err)
+
+				statusRegex := regexp.MustCompile(`"Status"\s*:\s*"passing"`)
+				if statusRegex.FindAllString(out, 2) == nil {
+					r.Errorf("Check status not yet passing")
+				}
+			})
+
+			//`serverServiceName` has a backing consul-dataplane check.
+			retry.RunWith(&retry.Timer{Timeout: 2 * time.Minute, Wait: 20 * time.Second}, t, func(r *retry.R) {
+				out, err := helpers.ExecuteRemoteCommand(
+					t, cfg, c.ecsClusterARN, consulServerTaskARN, "consul-server",
+					fmt.Sprintf(`/bin/sh -c 'curl %s localhost:8500/v1/health/checks/%s_%s'`, tokenHeader, serverServiceName, randomSuffix),
+				)
+				r.Check(err)
+
+				statusRegex := regexp.MustCompile(`"Status"\s*:\s*"passing"`)
+				if statusRegex.FindAllString(out, 1) == nil {
+					r.Errorf("Check status not yet passing")
+				}
+			})
+
+			// Use aws exec to curl between the apps.
+			tasks, err := helpers.ListTasks(t, c.ecsClusterARN, cfg.Region, fmt.Sprintf("Test_Client_%s", randomSuffix))
+
+			require.NoError(t, err)
+			require.Len(t, tasks.TaskARNs, 1)
+
+			testClientTaskARN := tasks.TaskARNs[0]
+			testClientTaskID := helpers.GetTaskIDFromARN(tasks.TaskARNs[0])
+
+			// Create an intention.
+			if c.secure {
+				// First check that connection between apps is unsuccessful.
+				retry.RunWith(&retry.Timer{Timeout: 3 * time.Minute, Wait: 20 * time.Second}, t, func(r *retry.R) {
+					curlCommand := fmt.Sprintf(`/bin/sh -c "curl http://%s_%s.service.consul"`, serverServiceName, randomSuffix)
+					curlOut, err := helpers.ExecuteRemoteCommand(t, cfg, c.ecsClusterARN, testClientTaskARN, "basic", curlCommand)
+					r.Check(err)
+					if !strings.Contains(curlOut, `curl: (52) Empty reply from server`) {
+						r.Errorf("response was unexpected: %q", curlOut)
+					}
+				})
+				retry.RunWith(&retry.Timer{Timeout: 6 * time.Minute, Wait: 20 * time.Second}, t, func(r *retry.R) {
+					consulCmd := fmt.Sprintf(`/bin/sh -c "consul intention create %s_%s %s_%s"`, clientServiceName, randomSuffix, serverServiceName, randomSuffix)
+					_, err := helpers.ExecuteRemoteCommand(t, cfg, c.ecsClusterARN, consulServerTaskARN, "consul-server", consulCmd)
+					r.Check(err)
+				})
+			}
+
+			retry.RunWith(&retry.Timer{Timeout: 3 * time.Minute, Wait: 20 * time.Second}, t, func(r *retry.R) {
+				curlCommand := fmt.Sprintf(`/bin/sh -c "curl http://%s_%s.service.consul"`, serverServiceName, randomSuffix)
+				curlOut, err := helpers.ExecuteRemoteCommand(t, cfg, c.ecsClusterARN, testClientTaskARN, "basic", curlCommand)
+				r.Check(err)
+				if !strings.Contains(curlOut, `"code": 200`) {
+					r.Errorf("response was unexpected: %q", curlOut)
+				}
+			})
+
+			helpers.StopTask(t, c.ecsClusterARN, cfg.Region, testClientTaskARN, "Stopped to validate graceful shutdown in acceptance tests")
+
+			// Wait for the task to stop (~30 seconds)
+			retry.RunWith(&retry.Timer{Timeout: 1 * time.Minute, Wait: 10 * time.Second}, t, func(r *retry.R) {
+				describeTasks, err := helpers.DescribeTasks(t, c.ecsClusterARN, cfg.Region, testClientTaskARN)
+
+				r.Check(err)
+				require.NotNil(r, describeTasks)
+				require.Len(r, describeTasks.Tasks, 1)
+				require.NotEqual(r, "RUNNING", describeTasks.Tasks[0].LastStatus)
+			})
+
+			// Check that the Envoy entrypoint received the sigterm.
+			retry.RunWith(&retry.Timer{Timeout: 2 * time.Minute, Wait: 30 * time.Second}, t, func(r *retry.R) {
+				envoyLogs, err := helpers.GetCloudWatchLogEvents(t, cfg, c.ecsClusterARN, testClientTaskID, "consul-dataplane")
+				require.NoError(r, err)
+
+				logMsg := "consul-ecs: waiting for application container(s) to stop"
+				envoyLogs = envoyLogs.Filter(logMsg)
+				require.GreaterOrEqual(r, len(envoyLogs), 1)
+				require.Contains(r, envoyLogs[0].Message, logMsg)
+			})
+
+			if c.secure {
+				retry.RunWith(&retry.Timer{Timeout: 2 * time.Minute, Wait: 30 * time.Second}, t, func(r *retry.R) {
+					// Validate that the controller cleans up the token for the failed task
+					syncLogs, err := helpers.GetCloudWatchLogEvents(t, cfg, c.ecsClusterARN, controllerTaskID, "consul-ecs-controller")
+					require.NoError(r, err)
+					syncLogs = syncLogs.Filter("token deleted successfully")
+					require.GreaterOrEqual(r, len(syncLogs), 1)
+				})
+			}
+
+			logger.Log(t, "Test successful!")
+		})
+	}
+}
---
+++ test/acceptance/tests/tproxy/terraform/main.tf
@@ -0,0 +1,419 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "ecs_cluster_arn" {
+  type        = string
+  description = "ARN of ECS cluster."
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC for all resources."
+  type        = string
+}
+
+variable "subnets" {
+  type        = list(string)
+  description = "Subnets to deploy into."
+}
+
+variable "suffix" {
+  type        = string
+  description = "Suffix to add to all resource names."
+}
+
+variable "region" {
+  type        = string
+  description = "Region."
+}
+
+variable "log_group_name" {
+  type        = string
+  description = "Name for cloudwatch log group."
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "secure" {
+  description = "Whether to create all resources in a secure installation (with TLS and ACLs)."
+  type        = bool
+  default     = false
+}
+
+
+variable "consul_license" {
+  description = "A Consul Enterprise license key. Requires consul_image to be set to a Consul Enterprise image."
+  type        = string
+  default     = ""
+  sensitive   = true
+}
+
+variable "consul_image" {
+  type    = string
+  default = ""
+}
+
+variable "launch_type" {
+  description = "Whether to launch tasks on Fargate or EC2"
+  type        = string
+}
+
+variable "consul_ecs_image" {
+  description = "Consul ECS image to use."
+  type        = string
+  default     = "ganeshrockz/ecs-tproxy:latest"
+}
+
+variable "server_service_name" {
+  description = "The service name for the test_server"
+  type        = string
+}
+
+variable "consul_datacenter" {
+  description = "The consul datacenter name. Should be unique among parallel test cases to ensure a unique Cloud Map namespace."
+  type        = string
+}
+
+provider "aws" {
+  region = var.region
+}
+
+locals {
+  enterprise_enabled = var.consul_license != ""
+}
+
+module "consul_server" {
+  source          = "../../../../../modules/dev-server"
+  lb_enabled      = false
+  ecs_cluster_arn = var.ecs_cluster_arn
+  subnet_ids      = var.subnets
+  vpc_id          = var.vpc_id
+  name            = "consul_server_${var.suffix}"
+  log_configuration = {
+    logDriver = "awslogs"
+    options = {
+      awslogs-group         = var.log_group_name
+      awslogs-region        = var.region
+      awslogs-stream-prefix = "consul_server_${var.suffix}"
+    }
+  }
+  launch_type = var.launch_type
+
+  tags = var.tags
+
+  tls  = var.secure
+  acls = var.secure
+
+  service_discovery_namespace = var.consul_datacenter
+  datacenter                  = var.consul_datacenter
+  consul_image                = var.consul_image
+  consul_license              = var.consul_license
+}
+
+data "aws_security_group" "vpc_default" {
+  vpc_id = var.vpc_id
+
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+}
+
+resource "aws_security_group_rule" "consul_server_ingress" {
+  description              = "Access to Consul dev server from default security group"
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = data.aws_security_group.vpc_default.id
+  security_group_id        = module.consul_server.security_group_id
+}
+
+module "ecs_controller" {
+  count  = var.secure ? 1 : 0
+  source = "../../../../../modules/controller"
+  log_configuration = {
+    logDriver = "awslogs"
+    options = {
+      awslogs-group         = var.log_group_name
+      awslogs-region        = var.region
+      awslogs-stream-prefix = "consul-ecs-controller"
+    }
+  }
+  launch_type                       = var.launch_type
+  consul_bootstrap_token_secret_arn = module.consul_server.bootstrap_token_secret_arn
+  consul_server_hosts               = module.consul_server.server_dns
+  consul_grpc_ca_cert_arn           = module.consul_server.ca_cert_arn
+  consul_https_ca_cert_arn          = module.consul_server.ca_cert_arn
+  ecs_cluster_arn                   = var.ecs_cluster_arn
+  region                            = var.region
+  subnets                           = var.subnets
+  name_prefix                       = var.suffix
+  consul_ecs_image                  = var.consul_ecs_image
+  consul_partitions_enabled         = local.enterprise_enabled
+  tls                               = var.secure
+}
+
+resource "aws_ecs_service" "test_client" {
+  name            = "test_client_${var.suffix}"
+  cluster         = var.ecs_cluster_arn
+  task_definition = module.test_client.task_definition_arn
+  desired_count   = 1
+  network_configuration {
+    subnets = var.subnets
+  }
+  launch_type            = var.launch_type
+  propagate_tags         = "TASK_DEFINITION"
+  enable_execute_command = true
+
+  tags = var.tags
+}
+
+module "test_client" {
+  source = "../../../../../modules/mesh-task"
+  // mesh-task will lower case this to `test_client_<suffix>` for the service name.
+  family                   = "Test_Client_${var.suffix}"
+  enable_transparent_proxy = true
+  enable_consul_dns        = true
+  requires_compatibilities = ["EC2"]
+  container_definitions = [
+    {
+      name             = "basic"
+      image            = "docker.mirror.hashicorp.services/buildpack-deps:jammy-curl"
+      essential        = true
+      logConfiguration = local.test_client_log_configuration
+      environment      = []
+      linuxParameters = {
+        initProcessEnabled = true
+      }
+      command = ["/bin/sh", "-c", "--", "while true; do sleep 100000; done;"]
+      healthCheck = {
+        command  = ["CMD-SHELL", "echo 1"]
+        interval = 30
+        retries  = 3
+        timeout  = 5
+      }
+    }
+  ]
+  consul_server_hosts = module.consul_server.server_dns
+  log_configuration   = local.test_client_log_configuration
+  outbound_only       = true
+  // This keeps the application running for 10 seconds.
+  application_shutdown_delay_seconds = 10
+  // Test with a port other than the default of 20000.
+  envoy_public_listener_port = 21000
+
+  tls                     = var.secure
+  consul_grpc_ca_cert_arn = var.secure ? module.consul_server.ca_cert_arn : ""
+  acls                    = var.secure
+  consul_ecs_image        = var.consul_ecs_image
+
+  additional_task_role_policies = [aws_iam_policy.execute-command.arn]
+
+  # For dev-server, the grpc_ca_cert (internal rpc) and the https ca cert are the same.
+  # But, they are different in HCP.
+  consul_https_ca_cert_arn = var.secure ? module.consul_server.ca_cert_arn : ""
+}
+
+resource "aws_ecs_service" "test_server" {
+  name            = "test_server_${var.suffix}"
+  cluster         = var.ecs_cluster_arn
+  task_definition = module.test_server.task_definition_arn
+  desired_count   = 1
+  network_configuration {
+    subnets = var.subnets
+  }
+  launch_type            = var.launch_type
+  propagate_tags         = "TASK_DEFINITION"
+  enable_execute_command = true
+
+  tags = var.tags
+}
+
+module "test_server" {
+  source                   = "../../../../../modules/mesh-task"
+  family                   = "test_server_${var.suffix}"
+  consul_service_name      = "${var.server_service_name}_${var.suffix}"
+  enable_transparent_proxy = true
+  enable_consul_dns        = true
+  requires_compatibilities = ["EC2"]
+  container_definitions = [{
+    name             = "basic"
+    image            = "docker.mirror.hashicorp.services/nicholasjackson/fake-service:v0.21.0"
+    essential        = true
+    logConfiguration = local.test_server_log_configuration
+  }]
+  consul_server_hosts = module.consul_server.server_dns
+  log_configuration   = local.test_server_log_configuration
+  port                = 9090
+
+  tls                     = var.secure
+  consul_grpc_ca_cert_arn = var.secure ? module.consul_server.ca_cert_arn : ""
+  acls                    = var.secure
+  consul_ecs_image        = var.consul_ecs_image
+
+  consul_https_ca_cert_arn = var.secure ? module.consul_server.ca_cert_arn : ""
+
+  // Test passing in roles. This requires users to correctly configure the roles outside mesh-task.
+  create_task_role      = false
+  create_execution_role = false
+  task_role             = aws_iam_role.task
+  execution_role        = aws_iam_role.execution
+}
+
+// Configure a task role for passing in to mesh-task.
+resource "aws_iam_role" "task" {
+  name = "test_server_${var.suffix}_task_role"
+  path = "/consul-ecs/"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  // Terraform does not support managing individual tags for IAM roles yet.
+  // These tags must be set when a role is passed in to mesh-task, if acls are enabled.
+  tags = {
+    "consul.hashicorp.com.service-name" = "${var.server_service_name}_${var.suffix}"
+    "consul.hashicorp.com.namespace"    = ""
+  }
+}
+
+// Policy to allow iam:GetRole (required for auth method)
+resource "aws_iam_policy" "get-task-role" {
+  count = var.secure ? 1 : 0
+  name  = "test_server_${var.suffix}_get_task_role_policy"
+  path  = "/consul-ecs/"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iam:GetRole"
+      ],
+      "Resource": [
+        "${aws_iam_role.task.arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "get-task-role" {
+  count      = var.secure ? 1 : 0
+  role       = aws_iam_role.task.id
+  policy_arn = aws_iam_policy.get-task-role[count.index].arn
+}
+
+// Policy to allow `aws execute-command`
+resource "aws_iam_policy" "execute-command" {
+  name   = "ecs-execute-command-${var.suffix}"
+  path   = "/consul-ecs/"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssmmessages:CreateControlChannel",
+        "ssmmessages:CreateDataChannel",
+        "ssmmessages:OpenControlChannel",
+        "ssmmessages:OpenDataChannel"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "execute-command" {
+  role       = aws_iam_role.task.id
+  policy_arn = aws_iam_policy.execute-command.arn
+}
+
+resource "aws_iam_role" "execution" {
+  name = "test_server_${var.suffix}_execution_role"
+  path = "/consul-ecs/"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  inline_policy {
+    name   = "test_server_${var.suffix}_execution_role_policy"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+%{if var.secure~}
+    {
+      "Effect": "Allow",
+      "Action": [
+        "secretsmanager:GetSecretValue"
+      ],
+      "Resource": [
+        "${module.consul_server.ca_cert_arn}"
+      ]
+    },
+%{endif~}
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+  }
+}
+
+locals {
+  test_server_log_configuration = {
+    logDriver = "awslogs"
+    options = {
+      awslogs-group         = var.log_group_name
+      awslogs-region        = var.region
+      awslogs-stream-prefix = "test_server_${var.suffix}"
+    }
+  }
+
+  test_client_log_configuration = {
+    logDriver = "awslogs"
+    options = {
+      awslogs-group         = var.log_group_name
+      awslogs-region        = var.region
+      awslogs-stream-prefix = "test_client_${var.suffix}"
+    }
+  }
+}
