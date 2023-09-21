802c6a0dbca84ce322ae944385d69ac46d481d65
Ganeshrockz <ganesh.seetharaman@hashicorp.com>
2023-09-21 12:11:01 +0530

    Add tproxy acceptance tests

---
+++ test/acceptance/tests/tproxy/main_test.go
@@ -0,0 +1,18 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package tproxy
+
+import (
+	"os"
+	"testing"
+
+	testsuite "github.com/hashicorp/terraform-aws-consul-ecs/test/acceptance/framework/suite"
+)
+
+var suite testsuite.Suite
+
+func TestMain(m *testing.M) {
+	suite = testsuite.NewSuite(m)
+	os.Exit(suite.Run())
+}
