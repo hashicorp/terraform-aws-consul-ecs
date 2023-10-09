c76447f70daffbf4b4f6749999f21fa0959d2c1e
Ganeshrockz <ganesh.seetharaman@hashicorp.com>
2023-10-09 16:03:03 +0530

    Fix fmt

--- modules/dev-server/consul_1_14_compat.tf
+++ modules/dev-server/consul_1_14_compat.tf
@@ -4,5 +4,5 @@
 locals {
   // parse the consul version from the provided image: ["1", "12", "2"]
   # consul_image_version_parts = regex(":.*(\\d+)[.](\\d+)[.](\\d+)", var.consul_image)
-  is_consul_1_14_plus        = true//tonumber(local.consul_image_version_parts[1]) >= 14
+  is_consul_1_14_plus = true //tonumber(local.consul_image_version_parts[1]) >= 14
 }
