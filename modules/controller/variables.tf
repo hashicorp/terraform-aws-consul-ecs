8fa916c2c20d25c8e719a04d4313a571d3b21fa5
Ganesh S <ganesh.seetharaman@hashicorp.com>
2023-11-21 11:55:06 +0530

    Update image versions (#233)

--- modules/controller/variables.tf
+++ modules/controller/variables.tf
@@ -4,7 +4,7 @@
 variable "consul_ecs_image" {
   description = "consul-ecs Docker image."
   type        = string
-  default     = "hashicorp/consul-ecs:0.7.0-rc1"
+  default     = "hashicorppreview/consul-ecs:0.8.0-dev"
 }
 
 variable "ecs_cluster_arn" {
