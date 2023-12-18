e5a5f20d88851027659fe09948d41242995ab326
Ganeshrockz <ganesh.seetharaman@hashicorp.com>
2023-12-18 20:52:55 +0530

    Fix sameness group test

--- test/acceptance/examples/scenarios/service-sameness/main.go
+++ test/acceptance/examples/scenarios/service-sameness/main.go
@@ -102,7 +102,7 @@ func (s *sameness) Validate(t *testing.T, outputVars map[string]interface{}) {
 	// Ensure that the gateways are also ready
 	ensureServiceReadiness(t, consulClientOne, fmt.Sprintf("%s-dc1-default-mesh-gateway", s.name), nil)
 	ensureServiceReadiness(t, consulClientOne, fmt.Sprintf("%s-dc1-%s-mesh-gateway", s.name, dc1Part1PartitionApps.partition), &api.QueryOptions{Partition: dc1Part1PartitionApps.partition})
-	ensureServiceReadiness(t, consulClientTwo, fmt.Sprintf("%s-dc2-default-mesh-gateway", s.name), nil)
+	ensureServiceReadiness(t, consulClientTwo, fmt.Sprintf("%s-dc2-mesh-gateway", s.name), nil)
 
 	// Begin actual validation
 	clusterAppsList := []*apps{
