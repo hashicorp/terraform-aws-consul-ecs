d49559a12344cc78d23bba16dc5ffeb30ca4ddae
Ganesh S <ganesh.seetharaman@hashicorp.com>
2023-12-20 17:43:14 +0530

    Validation framework for terraform examples (#250)
    * Validation framework
    
    * Add explicit dependency for FARGATE
    
    * Fmt
    
    * Make examples non flaky
    
    * Add sameness
    
    * More fixes
    
    * Pivoted to a registry based framework
    
    * Moved all scenarios to use the registry pattern
    
    * Add validations for scenario registration
    
    * Efficiently parse terraform outputs
    
    * Update README
    
    * Add registry tests
    
    * Panic on registration errors

---
+++ test/acceptance/examples/scenarios/common/fake_service_test.go
@@ -0,0 +1,89 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package common
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetFakeServiceResponse(t *testing.T) {
+	cases := map[string]struct {
+		httpRespCode int
+		respStr      string
+		wantErr      bool
+		errStr       string
+		expectedResp *FakeServiceResponse
+	}{
+		"non success error code": {
+			httpRespCode: http.StatusInternalServerError,
+			wantErr:      true,
+		},
+		"json unmarshalling error": {
+			httpRespCode: http.StatusOK,
+			respStr:      `unsupported-response-format`,
+			wantErr:      true,
+			errStr:       "unmarshalling json",
+		},
+		"failed upstream call": {
+			httpRespCode: http.StatusOK,
+			respStr:      "{\n  \"name\": \"consul-ecs-example-client-app\",\n  \"uri\": \"/\",\n  \"type\": \"HTTP\",\n  \"ip_addresses\": [\n    \"169.254.172.2\",\n    \"10.0.3.165\"\n  ],\n  \"start_time\": \"2023-12-17T13:14:48.498808\",\n  \"end_time\": \"2023-12-17T13:14:48.505297\",\n  \"duration\": \"6.488534ms\",\n  \"body\": \"Hello World\",\n  \"upstream_calls\": {\n    \"http://localhost:1234\": {\n      \"uri\": \"http://localhost:1234\",\n      \"code\": -1,\n      \"error\": \"Error communicating with upstream service: Get \\\"http://localhost:1234/\\\": EOF\"\n    }\n  },\n  \"code\": 500\n}",
+			expectedResp: &FakeServiceResponse{
+				Body: "Hello World",
+				Code: 500,
+				UpstreamCalls: map[string]UpstreamCallResponse{
+					"http://localhost:1234": {
+						Code: -1,
+					},
+				},
+			},
+		},
+		"successful upstream call": {
+			httpRespCode: http.StatusOK,
+			respStr:      "{\n  \"name\": \"consul-ecs-example-client-app\",\n  \"uri\": \"/\",\n  \"type\": \"HTTP\",\n  \"ip_addresses\": [\n    \"169.254.172.2\",\n    \"10.0.3.165\"\n  ],\n  \"start_time\": \"2023-12-17T13:01:18.291103\",\n  \"end_time\": \"2023-12-17T13:01:18.306560\",\n  \"duration\": \"15.456478ms\",\n  \"body\": \"Hello World\",\n  \"upstream_calls\": {\n    \"http://localhost:1234\": {\n      \"name\": \"consul-ecs-example-server-app\",\n      \"uri\": \"http://localhost:1234\",\n      \"type\": \"HTTP\",\n      \"ip_addresses\": [\n        \"169.254.172.2\",\n        \"10.0.2.34\"\n      ],\n      \"start_time\": \"2023-12-17T13:01:18.304883\",\n      \"end_time\": \"2023-12-17T13:01:18.305302\",\n      \"duration\": \"418.719µs\",\n      \"headers\": {\n        \"Content-Length\": \"298\",\n        \"Content-Type\": \"text/plain; charset=utf-8\",\n        \"Date\": \"Sun, 17 Dec 2023 13:01:18 GMT\"\n      },\n      \"body\": \"Hello World\",\n      \"code\": 200\n    }\n  },\n  \"code\": 200\n}",
+			expectedResp: &FakeServiceResponse{
+				Body: "Hello World",
+				Code: 200,
+				UpstreamCalls: map[string]UpstreamCallResponse{
+					"http://localhost:1234": {
+						Name: "consul-ecs-example-server-app",
+						Body: "Hello World",
+						Code: 200,
+						IpAddresses: []string{
+							"169.254.172.2",
+							"10.0.2.34",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for name, c := range cases {
+		c := c
+		t.Run(name, func(t *testing.T) {
+			handler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				w.WriteHeader(c.httpRespCode)
+				_, _ = w.Write([]byte(c.respStr))
+			})
+			server := httptest.NewServer(handler)
+			t.Cleanup(server.Close)
+
+			resp, err := GetFakeServiceResponse(server.URL)
+			if c.wantErr {
+				require.Error(t, err)
+				if c.errStr != "" {
+					require.Contains(t, err.Error(), c.errStr)
+				}
+			} else {
+				require.NotNil(t, resp)
+				require.True(t, reflect.DeepEqual(resp, c.expectedResp))
+			}
+		})
+	}
+}
---
+++ test/acceptance/examples/scenarios/scenario.go
@@ -0,0 +1,63 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package scenarios
+
+import (
+	"fmt"
+	"testing"
+)
+
+// ScenarioRegistry helps us interact with the
+// actual registry that holds details about the scenarios.
+type ScenarioRegistry interface {
+	// Register registers a scenario into the registry
+	Register(ScenarioRegistration)
+
+	// Retrieve retrieves a scenario from the registry
+	Retrieve(name string) (ScenarioRegistration, error)
+}
+
+type TerraformInputVarsHook func() (map[string]interface{}, error)
+type ValidateHook func(*testing.T, []byte)
+
+// ScenarioRegistration is the struct we expect each individual
+// scenario to use and register themselves by providing valid
+// lifecycle hooks
+type ScenarioRegistration struct {
+	// The name of the scenario. This name should match the value
+	// of TEST_SCENARIO environment variable which the test uses
+	// to determine the scenario to run.
+	Name string
+
+	// The name of the example's folder under the `examples/` directory
+	FolderName string
+
+	// List of TF variables that needs to be supplied to the
+	// example's terraform config.
+	TerraformInputVars TerraformInputVarsHook
+
+	// Validate is the hook called when validations need to be performed on the deployment. This
+	// hook will only be called after a successful terraform apply.
+	Validate ValidateHook
+}
+
+func (r *ScenarioRegistration) validate() error {
+	if r.Name == "" {
+		return fmt.Errorf("scenario name cannot be empty")
+	}
+
+	if r.FolderName == "" {
+		return fmt.Errorf("scenario %s should have a folder name associated to it", r.Name)
+	}
+
+	if r.TerraformInputVars == nil {
+		return fmt.Errorf("scenario %s should provide hooks for providing terraform input variables", r.Name)
+	}
+
+	if r.Validate == nil {
+		return fmt.Errorf("scenario %s should provide hooks validating the deployment", r.Name)
+	}
+
+	return nil
+}
