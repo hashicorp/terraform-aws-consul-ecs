81a4a16bd1c566561d60fa4228aa55bb809191ec
Ganeshrockz <ganesh.seetharaman@hashicorp.com>
2024-02-16 15:39:38 +0530

    Bring back dev version for 0.7.x

--- modules/gateway-task/main.tf
+++ modules/gateway-task/main.tf
@@ -5,7 +5,7 @@ data "aws_region" "current" {}
 
 locals {
   // Must be updated for each release, and after each release to return to a "-dev" version.
-  version_string = "0.7.3"
+  version_string = "0.7.4-dev"
 
   consul_data_volume_name = "consul_data"
   consul_data_mount = {
