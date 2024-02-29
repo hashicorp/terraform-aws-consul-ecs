51c51c4bdc964b5fc8eabb60e86baf0b96a5f61b
Ganeshrockz <ganesh.seetharaman@hashicorp.com>
2024-02-29 13:53:49 +0530

    Prepare 0.8.0 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## Unreleased
+## 0.8.0 (Feb 29, 2024)
 
 BREAKING CHANGES
 * Following are the changes made to the task definitions for `mesh-task` and `gateway-task` submodules to react to the changes made in [this](https://github.com/hashicorp/consul-ecs/pull/211) PR.
--- examples/cluster-peering/gateway/variables.tf
+++ examples/cluster-peering/gateway/variables.tf
@@ -77,5 +77,5 @@ variable "additional_task_role_policies" {
 variable "consul_ecs_image" {
   description = "Consul ECS image to use in all tasks."
   type        = string
-  default     = "hashicorppreview/consul-ecs:0.8.0-dev"
+  default     = "hashicorp/consul-ecs:0.8.0"
 }
