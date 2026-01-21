eaaccc3ba7ac6ca2660ab0b5f7acfa6f84514c5b
Swapnil Kumar <swapnilkitis@gmail.com>
2026-01-21 23:45:16 +0530

    Add healthcheck

--- modules/dev-server/main.tf
+++ modules/dev-server/main.tf
@@ -164,6 +164,16 @@ resource "aws_ecs_task_definition" "this" {
         linuxParameters = {
           initProcessEnabled = true
         }
+        healthCheck = {
+          command = [
+            "CMD-SHELL",
+            "consul members -http-addr=http://localhost:8500 || exit 1"
+          ]
+          interval    = 30
+          timeout     = 5
+          retries     = 3
+          startPeriod = 60
+        }
         dependsOn = var.tls ? [
           {
             containerName = "tls-init"
