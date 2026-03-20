55fe24ab86c7eaf7e53af75f822b4cde09ce596c
paras-gupta2 <paras.gupta@hashicorp.com>
2026-03-20 16:19:18 +0530

    Update Consul to 1.22.5, Dataplane to 1.9.5, and pin GitHub Actions to SHAs

--- examples/locality-aware-routing/variables.tf
+++ examples/locality-aware-routing/variables.tf
@@ -26,7 +26,7 @@ variable "lb_ingress_ip" {
 variable "consul_ecs_image" {
   description = "Consul ECS image to use in all tasks."
   type        = string
-  default     = "public.ecr.aws/hashicorp/consul-ecs:0.9.3"
+  default     = "public.ecr.aws/hashicorp/consul-ecs:0.9.4"
 }
 
 variable "consul_server_startup_timeout" {
@@ -38,5 +38,5 @@ variable "consul_server_startup_timeout" {
 variable "consul_image" {
   description = "Consul Docker image."
   type        = string
-  default     = "hashicorp/consul-enterprise:1.21.5-ent"
+  default     = "hashicorp/consul-enterprise:1.22.5-ent"
 }
--- modules/mesh-task/variables.tf
+++ modules/mesh-task/variables.tf
@@ -145,13 +145,13 @@ variable "outbound_only" {
 variable "consul_ecs_image" {
   description = "consul-ecs Docker image."
   type        = string
-  default     = "public.ecr.aws/hashicorp/consul-ecs:0.9.3"
+  default     = "public.ecr.aws/hashicorp/consul-ecs:0.9.4"
 }
 
 variable "consul_dataplane_image" {
   description = "consul-dataplane Docker image."
   type        = string
-  default     = "hashicorp/consul-dataplane:1.8.2"
+  default     = "hashicorp/consul-dataplane:1.9.5"
 }
 
 variable "envoy_public_listener_port" {
