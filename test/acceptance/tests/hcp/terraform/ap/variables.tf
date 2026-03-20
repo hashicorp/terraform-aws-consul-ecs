46fad18246bc8c0d6ffc391951fcfc1ec67bdeb9
paras-gupta2 <paras.gupta@hashicorp.com>
2026-03-20 18:03:30 +0530

    Update GitHub Actions to latest versions and Consul to 1.20.6 with Dataplane 1.8.12 and ECS 0.9.4

--- test/acceptance/tests/hcp/terraform/ap/variables.tf
+++ test/acceptance/tests/hcp/terraform/ap/variables.tf
@@ -70,7 +70,7 @@ variable "consul_image" {
 variable "consul_ecs_image" {
   description = "Consul ECS image to use."
   type        = string
-  default     = "public.ecr.aws/hashicorp/consul-ecs:0.9.3"
+  default     = "public.ecr.aws/hashicorp/consul-ecs:0.9.4"
 }
 
 variable "consul_server_address" {
