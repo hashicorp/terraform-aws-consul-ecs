b617232c608e230698cc1b3a9b2c7a9fa6d0b758
paras-gupta2 <paras.gupta@hashicorp.com>
2026-04-13 14:34:38 +0530

    Revert hcp version downgrade

--- test/acceptance/setup-terraform/main.tf
+++ test/acceptance/setup-terraform/main.tf
@@ -1,23 +1,6 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    hcp = {
-      source  = "hashicorp/hcp"
-      version = "0.66.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.region
 }
