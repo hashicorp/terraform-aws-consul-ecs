0798c2196a4ef594c0daa8b8bffc99fa2ba97970
paras-gupta2 <paras.gupta@hashicorp.com>
2026-04-15 23:48:59 +0530

    Update consul, consul ent and consul ecs versions

--- test/acceptance/tests/basic/terraform/basic-install/main.tf
+++ test/acceptance/tests/basic/terraform/basic-install/main.tf
@@ -69,7 +69,7 @@ variable "launch_type" {
 variable "consul_ecs_image" {
   description = "Consul ECS image to use."
   type        = string
-  default     = "public.ecr.aws/hashicorp/consul-ecs:0.9.3"
+  default     = "public.ecr.aws/hashicorp/consul-ecs:0.9.4"
 }
 
 variable "server_service_name" {
