7355626bbeda1c1cc3eaedfd4b7558e56f835035
Paras Gupta <paras.gupta@hashicorp.com>
2026-04-15 13:51:20 +0530

    Backport of adding validations for networkPartitionResilienceConfig (#391)
    * addes validations
    
    * formatting fix
    
    ---------
    
    Co-authored-by: Mukul Anand <anandmukul93@gmail.com>

--- modules/mesh-task/variables.tf
+++ modules/mesh-task/variables.tf
@@ -353,7 +353,7 @@ variable "consul_ecs_config" {
     error_message = "Only the 'enableTagOverride' and 'weights' fields are allowed in consul_ecs_config.service."
     condition = alltrue([
       for key in keys(lookup(var.consul_ecs_config, "service", {})) :
-      contains(["enableTagOverride", "weights"], key)
+      contains(["enableTagOverride", "weights", "networkResilienceConfig"], key)
     ])
   }
 
@@ -371,6 +371,16 @@ variable "consul_ecs_config" {
       for service in [lookup(var.consul_ecs_config, "service", {})] : [
         for key in keys(lookup(service, "weights", {})) :
         contains(["passing", "warning"], key)
+      ]
+    ]))
+  }
+
+  validation {
+    error_message = "Only the 'interval', 'maxFailures', 'enforcingConsecutive5xx', and 'maxEjectionPercent' fields are allowed in consul_ecs_config.service.networkResilienceConfig.outlierDetection."
+    condition = alltrue(flatten([
+      for service in [lookup(var.consul_ecs_config, "service", {})] : [
+        for key in keys(lookup(lookup(service, "networkResilienceConfig", {}), "outlierDetection", {})) :
+        contains(["interval", "maxFailures", "enforcingConsecutive5xx", "maxEjectionPercent"], key)
       ]
     ]))
   }
