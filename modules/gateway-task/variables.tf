913ed5b7aeebcc31e7f9b4f966c5c34c2e539fd8
Kruti Baraiya <kruti.baraiya@hashicorp.com>
2026-07-08 23:14:44 +0530

    fix(ci): update to correct ecs image (#414)

--- modules/gateway-task/variables.tf
+++ modules/gateway-task/variables.tf
@@ -108,7 +108,7 @@ variable "skip_server_watch" {
 variable "consul_ecs_image" {
   description = "consul-ecs Docker image."
   type        = string
-  default     = "hashicorppreview/consul-ecs:0.10.0-dev"
+  default     = "hashicorppreview/consul-ecs:0.10.0"
 }
 
 variable "consul_dataplane_image" {
